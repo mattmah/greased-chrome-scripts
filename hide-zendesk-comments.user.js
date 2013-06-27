c19b93d47f6511af1f8660ee13c62ef3ba8c9520
Matthew Mah <mattmah@mmah-sevone.(none)>
2013-06-27 16:53:33 -0400

    first commit

---
+++ hide-zendesk-comments.user.js
@@ -0,0 +1,20 @@
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+  // Note, jQ replaces $ to avoid conflicts.
+  var sevone_support_links = jQ('a[rel=sevonesupport]').parent().parent().parent().css("display","none");
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
